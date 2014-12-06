422659426f3b0e57b179120204e08a08007503b5
James Mackenzie <jamesmack1@gmail.com>
2014-12-06 15:02:48 -0800

    Did some minor refactoring of server.

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ class ServerChannel(object, Channel):
     """
     def __init__(self, *args, **kwargs):
         Channel.__init__(self, *args, **kwargs)
-        self.id = str(self._server.NextId())
+        self.id = str(self._server.next_id())
         self._player_pos = [0, 0]
         self.p1 = None
         self.sprite = Starship()  # Each player needs a sprite representation
@@ -33,25 +33,43 @@ class ServerChannel(object, Channel):
         self.sprite.update(value)
         self._player_pos = self.sprite.rect.x, self.sprite.rect.y
 
-    def WhichPlayer(self):
+    def which_player(self):
         return str("p1") if self.p1 else str("p2")
 
-    def PassOn(self, data):
-        # pass on what we received to all connected clients
-        self._server.SendToAll(data)
+    def pass_on(self, data):
+        """
+        Pass data to all connected clients
+        :dict data: Data to forward to clients
+        :return: None
+        """
+        self._server.send_to_all(data)
 
     def Close(self):
-        self._server.DelPlayer(self)
+        """
+        PodSixNet-defined callback for closing connection.
+        :return:
+        """
+        self._server.delete_player(self)
 
     ##################################
     ### Network specific callbacks ###
     ##################################
 
     def Network_move(self, data):
+        """
+        Processes move data from client.
+        :dict data: Data from client.
+        :return: None
+        """
         self.player_pos = data['p_pos']
-        self.PassOn(data)
+        self.pass_on(data)
 
     def Network_fire(self, data):
+        """
+        Processes fire data from client.
+        :dict data: Data from client.
+        :return: None
+        """
         bullet = Bullet(self.sprite.angle)
         # Set the bullet so it is where the player is
         bullet.x = self.sprite.rect.center[0]
@@ -60,7 +78,13 @@ class ServerChannel(object, Channel):
         self.bullets.add(bullet)
 
     def Network_restart(self, data):
-        self._server.Restart()
+        """
+        Processes restart from client.
+        :dict data: Data from client.
+        :return: None
+        """
+        self._server.restart()
+
 
 class TinyServer(object, Server):
     channelClass = ServerChannel
@@ -74,18 +98,41 @@ class TinyServer(object, Server):
         self.waiting_player_list = deque()  # Make a FIFO queue for waiting clients (no limit to waiting clients)
         print 'Server launched'
 
-    def NextId(self):
-        self.id += 1
-        return self.id
+    ###########################
+    ### PodSixNet callbacks ###
+    ###########################
 
     def Connected(self, channel, addr):
+        """
+        PodSixNet-defined callback for when a cient connects.
+        :ServerChannel channel: Representation of player
+        :str addr: IP address of channel
+        :return: None
+        """
         if self.p1 and self.p2:
             channel.Send({"action": "init", "p": "full"})
             self.waiting_player_list.append(channel)
         else:
-            self.AddPlayer(channel)
+            self.add_player(channel)
 
-    def AddPlayer(self, player):
+    ########################
+    ### Server functions ###
+    ########################
+
+    def next_id(self):
+        """
+        Generates unique ID for each client.
+        :return: ID number for client
+        """
+        self.id += 1
+        return self.id
+
+    def add_player(self, player):
+        """
+        Adds new player.
+        :ServerChannel player: PLayer to add.
+        :return: None
+        """
         # Determine if P1 or P2
         if self.p1 is None:
             self.p1 = player
@@ -107,22 +154,27 @@ class TinyServer(object, Server):
         # Else if P2, notify P2 and send position data
         else:
             self.p2.Send({"action": "init", "p": 'p2'})
-            self.SendToAll({"action": "ready"})
+            self.send_to_all({"action": "ready"})
             # Only send position data from P1 -> P2
             loc = list(self.p1.player_pos)
             loc.append(self.p1.sprite.angle)
-            self.SendToAll({"action": "move", "p": "p1", "p_pos": loc})
+            self.send_to_all({"action": "move", "p": "p1", "p_pos": loc})
             self.ready = True
 
-    def DelPlayer(self, player):
+    def delete_player(self, player):
+        """
+        Deletes player from server.
+        :ServerChannel player: PLayer to delete.
+        :return:
+        """
         self.ready = False
         if self.p1 is player:
             self.p1 = None
-            self.SendToAll({"action": "player_left"})
+            self.send_to_all({"action": "player_left"})
             print "Deleted P1 (" + str(player.addr) + ")"
         elif self.p2 is player:
             self.p2 = None
-            self.SendToAll({"action": "player_left"})
+            self.send_to_all({"action": "player_left"})
             print "Deleted P2 (" + str(player.addr) + ")"
         elif player in self.waiting_player_list:
             self.waiting_player_list.remove(player)
@@ -130,9 +182,13 @@ class TinyServer(object, Server):
             print("ERROR: Can't delete player")
         # Pull waiting player from queue
         if self.waiting_player_list:
-            self.AddPlayer(self.waiting_player_list.popleft())
+            self.add_player(self.waiting_player_list.popleft())
 
-    def HandleBullets(self):
+    def handle_bullets(self):
+        """
+        Perform bullet handling here
+        :return: None
+        """
         # Check if there are bullets (if all bullets are cleared, still should update screen to clear bullets)
         player_had_bullets = True if (self.p1.bullets or self.p2.bullets) else False
 
@@ -141,35 +197,43 @@ class TinyServer(object, Server):
         self.p2.bullets.update()
 
         # Do collision detection
-        self.HandleBulletHits(self.p1)
-        self.HandleBulletHits(self.p2)
+        self.handle_bullet_hits(self.p1)
+        self.handle_bullet_hits(self.p2)
 
         # Generate position list
-        bullet_list = self.GenerateBulletLocs()
+        bullet_list = self.gen_bullet_locs()
 
         # Send new bullet lists
         if bullet_list or player_had_bullets:
-            self.SendToAll({"action": "bullets",
+            self.send_to_all({"action": "bullets",
                             "bullets": bullet_list,
                             "p1_health": self.p1.sprite.health,
                             "p2_health": self.p2.sprite.health})
 
         # If any of the players have died, let both players know
         if self.p1.sprite.health <= 0 or self.p2.sprite.health <= 0:
-            self.HandleDeath()
+            self.handle_death()
 
-    def HandleDeath(self):
+    def handle_death(self):
+        """
+        Process the death of a player.
+        :return: None
+        """
         if self.p1.sprite.health <= 0:
-            dead = self.p1.WhichPlayer()
+            dead = self.p1.which_player()
             print("P1 has died")
         else:
-            dead = self.p2.WhichPlayer()
+            dead = self.p2.which_player()
             print("P2 has died")
         # Send message to clients that a player has died
-        self.SendToAll({"action": "death", "p": dead})
+        self.send_to_all({"action": "death", "p": dead})
         self.ready = False
 
-    def Restart(self):
+    def restart(self):
+        """
+        Handle game restart.
+        :return: None
+        """
         if self.p1 and self.p2:
             self.p1.sprite.reset_health()
             self.p2.sprite.reset_health()
@@ -177,16 +241,20 @@ class TinyServer(object, Server):
             # Clear bullet lists
             self.p1.bullets.empty()
             self.p2.bullets.empty()
-            self.SendToAll({"action": "bullets",
+            self.send_to_all({"action": "bullets",
                             "bullets": list(),
                             "p1_health": self.p1.sprite.health,
                             "p2_health": self.p2.sprite.health})
 
             # Notify clients
-            self.SendToAll({"action": "restart"})
+            self.send_to_all({"action": "restart"})
             self.ready = True
 
-    def GenerateBulletLocs(self):
+    def gen_bullet_locs(self):
+        """
+        Generate locations of bullets to send to players.
+        :return: list of bullet locations
+        """
         bullet_locs = list()
         for player in {self.p1, self.p2}:
             for bullet in player.bullets:
@@ -198,7 +266,12 @@ class TinyServer(object, Server):
                 bullet_locs.append((bullet.rect.x, bullet.rect.y, bullet.angle))
         return bullet_locs
 
-    def HandleBulletHits(self, player):
+    def handle_bullet_hits(self, player):
+        """
+        Perform collision detection on bullets.
+        :ServerChannel player: Player to check number of hits on
+        :return: None
+        """
         if player.p1:
             other_player = self.p2
         else:
@@ -212,17 +285,26 @@ class TinyServer(object, Server):
             player.sprite.health -= 10
             bullet.kill()
 
-    def SendToAll(self, data):
+    def send_to_all(self, data):
+        """
+        Send data to all connected clients.
+        :param data: Data to send
+        :return: None
+        """
         if self.p1 is not None:
             self.p1.Send(data)
         if self.p2 is not None:
             self.p2.Send(data)
 
-    def Launch(self):
+    def launch_server(self):
+        """
+        Main server loop.
+        :return: None
+        """
         while True:
             self.Pump()
             if self.ready:
-                self.HandleBullets()
+                self.handle_bullets()
             sleep(0.001)  # 0.01, 0.0001?
 
 # Assign dummy SDL screen and init headless PyGame
@@ -237,4 +319,4 @@ if len(sys.argv) != 2:
 else:
     host, port = sys.argv[1].split(":")
     s = TinyServer(localaddr=(host, int(port)))
-    s.Launch()
+    s.launch_server()
